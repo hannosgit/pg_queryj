3fb812930b4eed400e3e8eeac8524a3dc96cecfc
Hannes Oswald <hannes.oswald97@gmail.com>
2023-07-24 14:03:35 +0200

    initial commit

---
+++ src/main/java/org/example/PgQuery.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import com.sun.jna.*;
+
+public interface PgQuery extends Library {
+    PgQuery INSTANCE = Native.load("pg_query", PgQuery.class);
+    PgQueryParseResult pg_query_parse(String sql);
+
+    void pg_query_free_parse_result(PgQueryParseResult result);
+
+}
+
