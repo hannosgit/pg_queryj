87e1ee872533376762adf8f8d231a11b593b93fc
hannes.oswald <hannes.oswald97@gmail.com>
2026-01-16 19:40:13 +0100

    add protobuf parse and deparse

--- src/main/java/com/hannos/PgParser.java
+++ src/main/java/com/hannos/PgParser.java
@@ -1,14 +1,18 @@
 package com.hannos;
 
 
+import com.hannos.ffm.PgQueryDeparseResult;
 import com.hannos.ffm.PgQueryError;
 import com.hannos.ffm.PgQueryParseResult;
+import com.hannos.ffm.PgQueryProtobuf;
+import com.hannos.ffm.PgQueryProtobufParseResult;
 
 import java.lang.foreign.Arena;
 import java.lang.foreign.MemorySegment;
+import java.lang.foreign.ValueLayout;
 
-import static com.hannos.ffm.pg_query_h.pg_query_free_parse_result;
-import static com.hannos.ffm.pg_query_h.pg_query_parse;
+import static com.hannos.ffm.pg_query_h.*;
+
 
 public class PgParser {
 
@@ -33,6 +37,52 @@ public class PgParser {
             String parseTreeStr = parseTree.getString(0);
             pg_query_free_parse_result(memorySegment);
             return new QueryParseResult(parseTreeStr);
+        }
+    }
+
+    public static String deparse(byte[] protobufParseTree) {
+        try (Arena arena = Arena.ofConfined()) {
+            MemorySegment protobufStruct = PgQueryProtobuf.allocate(arena);
+            MemorySegment dataSegment = arena.allocateFrom(ValueLayout.JAVA_BYTE, protobufParseTree);
+            PgQueryProtobuf.len(protobufStruct, protobufParseTree.length);
+            PgQueryProtobuf.data(protobufStruct, dataSegment);
+
+            MemorySegment result = pg_query_deparse_protobuf(arena, protobufStruct);
+
+            MemorySegment errorPtr = PgQueryDeparseResult.error(result);
+            if (!errorPtr.equals(MemorySegment.NULL)) {
+                MemorySegment error = errorPtr.reinterpret(PgQueryError.sizeof(), arena, null);
+                String message = PgQueryError.message(error).getString(0);
+                int cursorPos = PgQueryError.cursorpos(error);
+                pg_query_free_deparse_result(result);
+                throw new ParseException(message, cursorPos);
+            }
+
+            String query = PgQueryDeparseResult.query(result).getString(0);
+            pg_query_free_deparse_result(result);
+            return query;
+        }
+    }
+
+    public static byte[] parseProtobuf(String sql) {
+        try (Arena arena = Arena.ofConfined()) {
+            MemorySegment result = pg_query_parse_protobuf(arena, arena.allocateFrom(sql));
+
+            MemorySegment errorPtr = PgQueryProtobufParseResult.error(result);
+            if (!errorPtr.equals(MemorySegment.NULL)) {
+                MemorySegment error = errorPtr.reinterpret(PgQueryError.sizeof(), arena, null);
+                String message = PgQueryError.message(error).getString(0);
+                int cursorPos = PgQueryError.cursorpos(error);
+                pg_query_free_protobuf_parse_result(result);
+                throw new ParseException(message, cursorPos);
+            }
+
+            MemorySegment parseTree = PgQueryProtobufParseResult.parse_tree(result);
+            int len = PgQueryProtobuf.len(parseTree);
+            MemorySegment dataPtr = PgQueryProtobuf.data(parseTree).reinterpret(len, arena, null);
+            byte[] protobufBytes = dataPtr.toArray(ValueLayout.JAVA_BYTE);
+            pg_query_free_protobuf_parse_result(result);
+            return protobufBytes;
         }
     }
 
